6c46853490c4e586f7b3b6185b62d90f0a13ea11
stan <stann84@hotmail.com>
2018-04-30 00:12:11 +0200

    tout qui marche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  posts = [
+
+  post1 = 'Mon premier post';
+  lovep1 = '4';
+  //count = 4;
+  post2 = 'Mon second post';
+  lovep2 = '-4';
+  post3 = 'Encore un post';
+  lovep3 = '0';
+
+
+
+
+
+
+
+
+  /*count() {
+    console.log('allumer');
+  }*/
+
+
+  /*posts = [
     {
       title : 'mon premier post',
       content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
       loveits : '4',
       created_at : "10/10/10"
     }
-  ]
+  ]*/
 }
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.scss']
 })
 export class PostListItemComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveits: number;
+  @Input() count: number;
+
+  content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  created_at = new Date();
+  count = 0;
 
   constructor() { }
 
