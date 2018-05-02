e808690600b2cdca208df7e56d30b3d650b64aa2
stan <stann84@hotmail.com>
2018-05-02 12:41:14 +0200

    suppresion du dossier post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 
@@ -11,7 +10,6 @@ import { PostListItemComponent } from './post-list-item/post-list-item.component
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
     PostListComponent,
     PostListItemComponent
   ],
